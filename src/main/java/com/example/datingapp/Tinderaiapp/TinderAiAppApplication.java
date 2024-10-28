b7b99003f3beacc756c90b71d258ede53f47e4aa
FATHIMATH SUNITHA <fathimathsunitha4403@gmail.com>
2024-10-28 13:37:12 +0530

    apikey changes

---
+++ src/main/java/com/example/datingapp/Tinderaiapp/TinderAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.datingapp.Tinderaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datingapp/Tinderaiapp/TinderAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingapp.Tinderaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
